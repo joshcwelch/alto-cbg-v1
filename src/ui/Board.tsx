71e11c2bbb197635afd0f1584db0bd5c787e05f6
Josh <joshcwelch@gmail.com>
2025-12-07 14:42:38 -0600

    Refactor Board component: remove unused GameScene and BoardBG, integrate UIHud for player stats, and adjust Hand component layout for improved responsiveness

--- src/ui/Board.tsx
+++ src/ui/Board.tsx
@@ -1,12 +1,9 @@
-import { Canvas } from "@react-three/fiber";
 import { useEffect } from "react";
-import GameScene from "./GameScene";
-import EndTurnButton from "./EndTurnButton";
-import ManaBar from "./ManaBar";
 import { useGameStore } from "../state/useGameStore";
 import BoardStage from "./BoardStage";
 import Battlefield from "./Battlefield";
 import Hand from "./Hand";
+import UIHud from "./UIHud";
 
 export default function Board() {
   const newGame = useGameStore(s => s.newGame);
@@ -17,20 +14,9 @@ export default function Board() {
 
   return (
     <BoardStage>
-      <Canvas
-        shadows
-        orthographic
-        gl={{ alpha: true }}
-        dpr={[1, 2]}
-        camera={{ zoom: 100, position: [0, 0, 10] }}
-        style={{ position: "absolute", inset: 0, width: "100%", height: "100%" }}
-      >
-        <GameScene />
-      </Canvas>
       <Battlefield />
       <Hand />
-      <ManaBar />
-      <EndTurnButton />
+      <UIHud />
     </BoardStage>
   );
 }
