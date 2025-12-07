eb256bb7b01eb926c0f0a759cd2171b1476256e3
Josh <joshcwelch@gmail.com>
2025-12-07 12:38:14 -0600

    Refactor Board3D component: update texture import and simplify board dimensions

--- src/ui/Board3D.tsx
+++ src/ui/Board3D.tsx
@@ -1,18 +1,16 @@
 import { useTexture } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import { SRGBColorSpace } from "three";
+import boardImg from "../assets/board/board.png";
 
-const BOARD_PATH = "/src/assets/board/board.png";
-const BOARD_ASPECT = 1152 / 2048; // approximate height / width from provided asset
-const BASE_WIDTH = 12;
-const BOARD_SCALE = 1.35;
-const BOARD_WIDTH = BASE_WIDTH * BOARD_SCALE;
+const BOARD_WIDTH = 12;
+const BOARD_ASPECT = 1152 / 2048; // height / width from provided asset
 const BOARD_HEIGHT = BOARD_WIDTH * BOARD_ASPECT;
 const BOARD_TILT = -0.42;
 
 export default function Board3D() {
   const { gl } = useThree();
-  const boardTex = useTexture(BOARD_PATH);
+  const boardTex = useTexture(boardImg);
 
   if (boardTex) {
     boardTex.colorSpace = SRGBColorSpace;
@@ -25,7 +23,6 @@ export default function Board3D() {
       renderOrder={0}
       position={[0, 0, 0]}
       rotation={[BOARD_TILT, 0, 0]}
-      scale={[6, 4, 1]}
       receiveShadow
     >
       <planeGeometry args={[BOARD_WIDTH, BOARD_HEIGHT]} />
